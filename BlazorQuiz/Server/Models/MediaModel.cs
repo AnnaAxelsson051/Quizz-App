8e08c186260a21ae572c230b56d4e73fddf62b49
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-01 18:38:03 +0200

    Add models

---
+++ BlazorQuiz/Server/Models/MediaModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorQuiz.Server.Models
+{
+    // Fixad
+    // Representing media entities
+
+    public class MediaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Stores a globally unique identifier for the media item
+        public Guid Guid { get; set; }
+        public string Type { get; set; } // Type of media
+        public string Description { get; set; }
+        public string Path { get; set; } // Path to access the media content
+
+        [ForeignKey("User")]
+        public string UserRefId { get; set; }
+
+        // To access associated ApplicationUser entity,for navigation from
+        // media items to their resp users
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ BlazorQuiz/Server/Models/QuestionModel.cs
@@ -0,0 +1,51 @@
+﻿using BlazorQuiz.Shared.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+// Fixad
+
+namespace BlazorQuiz.Server.Models
+{
+    public class QuestionModel
+    {
+
+        public QuestionModel() { }
+        public QuestionModel(NewQuestionViewModel input)
+        {
+            
+            Question = input.Question;
+            Answer1 = input.Answer1;
+            Answer2 = input.Answer2;
+            Answer3 = input.Answer3;
+            Answer4 = input.Answer4;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Question { get; set; } // Question text
+        public string Answer1 { get; set; }  // Answer options
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+
+        // QuizRefId is foreign referencing
+        // QuizModel entity
+
+        [ForeignKey("Quiz")]
+        public string QuizRefId { get; set; }
+
+        // Quiz property should be ignored during JSON serialization
+        // Nav prop named Quiz of type QuizModel
+        // represents associated quiz entity
+
+        [JsonIgnore]
+        public virtual QuizModel Quiz { get; set; }
+
+        // MediaRefId is foreign key that references MediaModel entity
+
+        [ForeignKey("Media")]
+        public int? MediaRefId { get; set; }
+        public virtual MediaModel Media { get; set; }
+    }
+}
