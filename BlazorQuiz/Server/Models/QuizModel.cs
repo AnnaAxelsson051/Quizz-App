10a79d577e00e9149baed13144193d3be4da8834
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-01 17:35:01 +0200

    Add quiz model

---
+++ BlazorQuiz/Server/Models/QuizModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using IdentityModel;
+
+// Fixad
+
+namespace BlazorQuiz.Server.Models
+{
+    public class QuizModel
+    {
+        public int Id { get; set; }
+
+        // PublicId is primary key for the entity
+
+        [Key]
+        public string PublicId { get; set; }
+        public string Name { get; set; }  // Name of quiz
+        public int Timer { get; set; }
+
+        // UserRefId is foreign key that references 
+        // ApplicationUser entity / for the associated user
+
+        [ForeignKey("User")]
+        public string UserRefId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        // Collection of questions
+
+        public ICollection<QuestionModel> Questions { get; set; }
+    }
+}
