fda2ef2c921aa0c855c6e88cb3ef886118877803
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-03 18:36:12 +0200

    Add functionality for uploading media files

---
+++ BlazorQuiz/Server/Controllers/MediaController.cs
@@ -0,0 +1,51 @@
+﻿using BlazorQuiz.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+using BlazorQuiz.Shared.ViewModels;
+using BlazorQuiz.Server.Models;
+using BlazorQuiz.Server.Services;
+using BlazorQuiz.Server.Interfaces;
+
+namespace BlazorQuiz.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : BaseController
+    {
+        private readonly IMediaService _mediaService;
+
+        public MediaController(IMediaService mediaService )
+        {
+            _mediaService = mediaService;
+        }
+
+        // Uploading media files, perform check for file size and type before saving
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMediaAsync([FromForm] IFormFile file)
+        {
+            long megaByteSize = 1024 * 1024;
+            int maxFileSizeInMB = 13;
+           
+            long maxAllowedSizeInBytes = maxFileSizeInMB * megaByteSize;
+            string[] allowedFileExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".mp4" };
+            var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (file.Length > maxAllowedSizeInBytes)
+            {
+                return BadRequest("The size of the file you are attemting to upload is to large");
+            }
+
+            if (string.IsNullOrEmpty(fileExtension) || !allowedFileExtensions.Contains(fileExtension))
+            {
+                return BadRequest("The file type: " + file.ContentType + " is not a valid filetype");
+            }
+
+            var uploadedMedia = await _mediaService.UploadMediaAsync(file, UserId);
+
+            return Ok(uploadedMedia);
+        }
+
+       
+
+    }
+}
