36d34f2119ecc2c26d67d1cc6838c8b94a43289d
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-04 14:30:45 +0200

    Add files

---
+++ BlazorQuiz/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorQuiz.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorQuiz.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<QuizModel> Quizzes { get; set; }
+        public DbSet<UserQuizModel> UserQuizModels { get; set; }
+        public DbSet<QuestionModel> QuestionModels { get; set; }
+        public DbSet<MediaModel> MediaModels { get; set; }
+
+    }
+}
