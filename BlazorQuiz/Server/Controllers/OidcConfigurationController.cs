d828e525f6d7f863da537978d4bd7501afb24582
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-08 14:41:41 +0200

    Add functionality for secure login process providing client request parameters

---
+++ BlazorQuiz/Server/Controllers/OidcConfigurationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorQuiz.Server.Controllers
+{
+    public class OidcConfigurationController : Controller
+    {
+        private readonly ILogger<OidcConfigurationController> _logger;
+
+        public OidcConfigurationController(IClientRequestParametersProvider clientRequestParametersProvider, ILogger<OidcConfigurationController> logger)
+        {
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+            _logger = logger;
+        }
+
+        // Retrieves client specific request parameters 
+
+        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
+
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+        {
+            var clientParameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+            return Ok(clientParameters);
+        }
+    }
+}
