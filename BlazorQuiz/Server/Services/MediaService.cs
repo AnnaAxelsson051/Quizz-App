9c59df2ec4b5efb0d67fdd0be2bf6d9f74f598e5
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-08 14:49:05 +0200

    Modify file upload functionalities add functionality for saving uploaded file

--- BlazorQuiz/Server/Services/MediaService.cs
+++ BlazorQuiz/Server/Services/MediaService.cs
@@ -50,6 +50,58 @@ namespace BlazorQuiz.Server.Services
             return mediaViewModel;
 
         }
+ // Saves an uploaded file based on type (image or video),
+        // determines the destination directory based on the file's content type (image or video),
+        // creates the appropriate directory if doesn't exist
+        // saves the file to location with a unique file name
+        // returning the relative path to the saved file
+
+        private async Task<string> SaveFileAsync(IFormFile file, string fileName)
+        {
+            string imageDirectoryPath = "wwwroot/images";
+            string videoDirectoryPath = "wwwroot/videos";
+            string destinationDirectory = imageDirectoryPath;
+
+            if (file.ContentType.StartsWith("video"))
+            {
+                bool videoDirectoryExists = Directory.Exists(videoDirectoryPath);
+                if (!videoDirectoryExists)
+                {
+                    Directory.CreateDirectory(videoDirectoryPath);
+                }
+                destinationDirectory = videoDirectoryPath;
+            }
+            else if(file.ContentType.StartsWith("image"))
+            {
+                bool imageDirectoryExists = Directory.Exists(imageDirectoryPath);
+                if (!imageDirectoryExists)
+                {
+                    Directory.CreateDirectory(imageDirectoryPath);
+                }
+            }
+            Directory.CreateDirectory(fileName);
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), destinationDirectory, fileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            destinationDirectory = destinationDirectory.Replace("wwwroot/", "");
+            var uploadedFilePath = Path.Combine(destinationDirectory, fileName);
+
+            uploadedFilePath = uploadedFilePath.Replace("\\", "/");
+
+            return uploadedFilePath;
+        }
+
+        // Retrieves a media file from the database based on its GUID
+
+        public async Task<MediaModel> GetMediaByIdAsync(Guid guid)
+        {
+            var media = _context.MediaModels.Where(file => file.Guid == guid).Single();
+
+            return media;
+        }
 
        
     }
