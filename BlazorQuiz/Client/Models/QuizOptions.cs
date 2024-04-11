34d876b4fff9fbdf782be00072f746c6c7008997
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-11 18:44:15 +0200

    Implement file upload functionality with error handling and feedback messages

---
+++ BlazorQuiz/Client/Models/QuizOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorQuiz.Client.Models
+{
+    public class QuizOptions
+    {
+        public double Timer { get; set; } = 60;
+        public string QuizTitle { get; set; }
+        public bool HasTimer { get; set; }
+        public bool MediaQuestion { get; set; }
+        public bool IsValidated { get; set; }
+        public bool HasQuestion { get; set; }
+        public string QuizGuid { get; set; }
+
+
+        public void ResetToDefault()
+        {
+            MediaQuestion = false;
+        }
+        public void NewGame()
+        {
+            QuizGuid = string.Empty;
+            QuizTitle = string.Empty;
+            Timer = 60;
+            HasQuestion = false;
+            IsValidated = false;
+            HasTimer = false;
+        }
+    }
+}
