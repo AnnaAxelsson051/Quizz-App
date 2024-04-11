002b9f7e760cbb1fc8a66ffdd4cac2023ab7ac10
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-11 19:02:29 +0200

    Add new migrations

---
+++ BlazorQuiz/Server/Data/Migrations/20240411185819_nullable mediaref.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorQuiz.Server.Migrations
+{
+    public partial class nullablemediaref : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionModels_MediaModels_MediaRefId",
+                table: "QuestionModels");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MediaRefId",
+                table: "QuestionModels",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionModels_MediaModels_MediaRefId",
+                table: "QuestionModels",
+                column: "MediaRefId",
+                principalTable: "MediaModels",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionModels_MediaModels_MediaRefId",
+                table: "QuestionModels");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MediaRefId",
+                table: "QuestionModels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionModels_MediaModels_MediaRefId",
+                table: "QuestionModels",
+                column: "MediaRefId",
+                principalTable: "MediaModels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
